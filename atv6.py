1096ad425c40d0bbb3da5d903bcd4d6e2cc717bf
sofymiranda <mirandaspfya@gmail.com>
2023-10-16 14:34:43 -0300

    atividade 6

---
+++ atv6.py
@@ -0,0 +1,27 @@
+# Digite um numero interio e veja sua tabuada
+print ("Digite um numero interio e veja sua tabuada:")
+numero= float (input("digite o numero:"))
+f1 = numero * 1
+f2 = numero * 2
+f3 = numero *3
+f4 = numero *4
+f5 = numero *5
+f6 =  numero * 6
+f7 = numero * 7
+f8 = numero * 8
+f9 = numero * 9
+f10= numero * 10
+print (f""" 
+ a sua tabuada é:
+       {f1}  
+       {f2}    
+       {f3}
+       {f4}
+       {f5}
+       {f6}
+       {f7}
+       {f8}
+       {f9}
+       {f10}""")
+
+
